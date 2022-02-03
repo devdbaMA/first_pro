4ed9de4905310b853daad595e8b6c0b35f74f317
Makhmud.A <efendiyev.mahmud@gmail.com>
2022-02-03 11:26:27 +0400

    Initial commit

---
+++ src/test/java/tckmng/first_pro/FirstProApplicationTests.java
@@ -0,0 +1,13 @@
+package tckmng.first_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
